d2579e9e7ef310f269cc7b5337bde0f39a4f77ef
harryg02 <94257270+harryg02@users.noreply.github.com>
2026-04-23 12:27:11 -0400

    feat: add item-moving functionality with UI list elements to index page

--- js/script.js
+++ js/script.js
@@ -8,3 +8,13 @@ if (typeof module !== 'undefined') {
   }
 }
 
+document.querySelector('#move-the-items').addEventListener('click', () => {
+  // @todo this is the callback that fires when you click the "Move Items" button.
+  // Reference the working example and get it working the same way.
+  // This must match the approach of the working example, you're being graded on your ability
+  // to figure that out.
+  if (document.querySelector('#start-list > li') !== null) {
+    document.querySelector('#end-list').append(document.querySelector('#start-list > li'))
+  }
+})
+
